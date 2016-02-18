58d7ce4614bc9a107705a6cd0f71da41e2fe808a
songaw <50330690@qq.com>
2016-02-18 12:46:53 +0800

    aa

--- src/test/java/com/anwei/rms/service/RoleServiceTest.java
+++ src/test/java/com/anwei/rms/service/RoleServiceTest.java
@@ -23,6 +23,7 @@ public class RoleServiceTest {
 */
 	public static void main(String[] args) {
 		System.out.println("sd");
+		System.out.println("sd");
 	/*	ApplicationContext ac=new ClassPathXmlApplicationContext("/config/spring.xml");
 		RoleService rc=(RoleService) ac.getBean(RoleService.class);
 		RoleDomain roleDomain = new RoleDomain();
